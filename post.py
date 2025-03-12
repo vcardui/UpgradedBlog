c36dfa63b2a9ba8e4cc900e58753ac23051b865a
Vanessa Reteguín <145515264+vcardui@users.noreply.github.com>
2025-03-11 22:02:34 -0600

    Update post.py

--- post.py
+++ post.py
@@ -1,5 +1,21 @@
+# +----------------------------------------------------------------------------+
+# | CARDUI WORKS v1.0.0
+# +----------------------------------------------------------------------------+
+# | Copyright (c) 2024 - 2025, CARDUI.COM (www.cardui.com)
+# | Vanessa Reteguín <vanessa@reteguin.com>
+# | Released under the MIT license
+# | www.cardui.com/carduiframework/license/license.txt
+# +----------------------------------------------------------------------------+
+# | Author.......: Vanessa Reteguín <vanessa@reteguin.com>
+# | First release: January 9th, 2025
+# | Last update..: March 11th, 2025
+# | WhatIs.......: Cardui Web Development Post - Class
+# +----------------------------------------------------------------------------+
+
+# ------------------------- Libraries -------------------------
 import requests
 
+# -------------------------- Class ----------------------------
 class Post:
     def __init__(self, id):
         self.id = int(id) - 1
